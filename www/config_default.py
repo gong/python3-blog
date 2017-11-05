19420b775995a34aabffd76fceccfd085e8d9829
dangdang <genzhedangdang@gmail.com>
2017-11-05 21:09:57 +0800

    Update config_default.py

--- www/config_default.py
+++ www/config_default.py
@@ -2,8 +2,8 @@ configs = {
     'db': {
         'host': '127.0.0.1',
         'port': 3306,
-        'user': 'root',
-        'password': '1143178769',
+        'user': '****',
+        'password': '********',
         'database': 'webapp'
     },
     'session': {
