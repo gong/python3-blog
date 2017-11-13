ba2ba11700e37e8d3c5abfcb16c62541d6c4c5bb
gongxin <genzhedangdang@gmail.com>
2017-11-13 11:32:00 +0800

    添加github.css

--- www/config_default.py
+++ www/config_default.py
@@ -2,8 +2,8 @@ configs = {
     'db': {
         'host': '127.0.0.1',
         'port': 3306,
-        'user': '****',
-        'password': '********',
+        'user': 'root',
+        'password': '1143178769',
         'database': 'webapp'
     },
     'session': {
